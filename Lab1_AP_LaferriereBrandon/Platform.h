1c7464f4a5d8f345a9bbb4e3a3e105759b619122
Brandon <bmlaferriere@gmail.com>
2021-01-18 22:59:34 -0500

    Add project files.

---
+++ Lab1_AP_LaferriereBrandon/Platform.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Platform
+{
+private:
+	string m_platName;
+	string m_platManu;
+	string m_platGames[];
+public:
+
+	Platform(string platName, string platManu,)
+	string getPlatName();
+	string setPlatName();
+	string getPlatManu();
+	string setPlatManu();
+	string getPlatGames();
+	string setPlatGames();
+
+};
+
