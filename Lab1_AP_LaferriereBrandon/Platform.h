e1861d2d64e436a31365d3d1bec653f73e05e8db
Brandon <bmlaferriere@gmail.com>
2021-01-19 10:05:01 -0500

    changed header

--- Lab1_AP_LaferriereBrandon/Platform.h
+++ Lab1_AP_LaferriereBrandon/Platform.h
@@ -8,16 +8,38 @@ class Platform
 private:
 	string m_platName;
 	string m_platManu;
+	int m_size = 0;
 	string m_platGames[];
+	
 public:
-
-	Platform(string platName, string platManu,)
-	string getPlatName();
-	string setPlatName();
-	string getPlatManu();
-	string setPlatManu();
+	string getPlatName()
+	{
+		return m_platName;
+	}
+	void setPlatName(string platName)
+	{
+		m_platName = platName;
+	}
+	string getPlatManu()
+	{
+		return m_platManu;
+	}
+	void setPlatManu(string platManu)
+	{
+		m_platManu = platManu;
+	}
 	string getPlatGames();
-	string setPlatGames();
+	{
+		
+	}
+	void setPlatGames(string platGames[])
+	{
+		for (int i = 0; i > m_size; i++)
+		{
+			m_platGames[i] = platGames[i];
+		}
+		
+	}
 
 };
 
